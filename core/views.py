63b62fc6db00bb57c131fb447e4d1173e68f0334
Dhruv Kar <dhruv@wints.org>
2020-08-20 15:23:54 -0500

    fix require_POST import

--- core/views.py
+++ core/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-from django.views.decorators.http import require_http_methods
+from django.views.decorators.http import require_http_methods, require_POST
 
 from twilio.twiml.messaging_response import MessagingResponse
 
