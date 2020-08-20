7ab204e9296385a933f2eaec0203396d1afac9d4
Dhruv Kar <dhruv@wints.org>
2020-08-20 15:19:46 -0500

    change sms url to POST only

--- core/views.py
+++ core/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_http_methods
+
 from twilio.twiml.messaging_response import MessagingResponse
 
 
@@ -10,6 +12,7 @@ def sms_response(request):
     return HttpResponse("Hello, world.")
 
 @csrf_exempt
+@require_POST
 def sms_reply(self):
 
     response = MessagingResponse()
